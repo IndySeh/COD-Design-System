748fe2d240a02833440b69b4228a424dfb3eda1a
Max Morgan <maxwell.morgan@detroitmi.gov>
2024-06-28 13:06:58 -0400

    Update stories to provide sensible parent container sizes

--- src/stories/legacyicon.stories.js
+++ src/stories/legacyicon.stories.js
@@ -23,7 +23,7 @@ export default {
   },
   args: {
     icon: 'neighborhoods',
-    outlineColor: 'black',
+    outlineColor: '#18252A',
   },
 };
 
@@ -36,7 +36,11 @@ function _createLegacyIcon(args) {
 
 // Template
 const Template = (args) => {
-  return _createLegacyIcon(args);
+  return html`
+    <div class="w-50 h-50">
+      ${_createLegacyIcon(args)}
+    </div>
+  `;
 };
 
 export const Primary = Template.bind({});
@@ -44,14 +48,20 @@ export const Primary = Template.bind({});
 export const IconGrid = () => {
   return html`
     <div class="row">
-      <div class="col px-0 d-flex justify-content-center">
-        <cod-legacy-icon icon="neighborhoods"> </cod-legacy-icon>
+      <div class="col-lg-4 col-sm-12 col-md-6 px-0 d-flex justify-content-center align-items-center">
+        <div class="w-75 h-75">
+          <cod-legacy-icon icon="neighborhoods"> </cod-legacy-icon>
+        </div>
       </div>
-      <div class="col px-0 d-flex justify-content-center">
-        <cod-legacy-icon icon="people"> </cod-legacy-icon>
+      <div class="col-lg-4 col-sm-12 col-md-6 px-0 d-flex justify-content-center align-items-center">
+        <div class="w-75 h-75">
+          <cod-legacy-icon icon="people"> </cod-legacy-icon>
+        </div>
       </div>
-      <div class="col px-0 d-flex justify-content-center">
-        <cod-legacy-icon icon="sustainability"> </cod-legacy-icon>
+      <div class="col-lg-4 col-sm-12 col-md-6 px-0 d-flex justify-content-center align-items-center">
+        <div class="w-75 h-75">
+          <cod-legacy-icon icon="sustainability"> </cod-legacy-icon>
+        </div>
       </div>
     </div>
   `;
